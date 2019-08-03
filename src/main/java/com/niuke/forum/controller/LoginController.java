4397d0fe579cc787688b91eb6a8bb90eea3fc02e
sssal <1225941299@qq.com>
2019-08-03 22:29:25 +0800

    wenda forum 03(mysql)

---
+++ src/main/java/com/niuke/forum/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.niuke.forum.controller;
+
+import com.niuke.forum.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(path = {"/reg"}, method = {RequestMethod.GET})
+    public String reg(Model model,
+                      @RequestParam("username") String username,
+                      @RequestParam("password") String password) {
+        try {
+            Map<String, String> map = userService.register(username, password);
+            if (map.containsKey("msg")) {
+                model.addAttribute("msg", map.get("msg"));
+                return "login";
+            }
+
+            return "redirect:/";
+        } catch (Exception e) {
+            logger.error("注册异常" + e.getMessage());
+            return "login";
+        }
+    }
+
+    @RequestMapping(path = {"/reglogin"}, method = {RequestMethod.GET})
+    public String reg(Model model) {
+        return "login";
+    }
+
+
+}
+
--- src/main/java/com/niuke/forum/service/UserService.java
+++ src/main/java/com/niuke/forum/service/UserService.java
@@ -2,8 +2,15 @@ package com.niuke.forum.service;
 
 import com.niuke.forum.dao.UserDAO;
 import com.niuke.forum.model.User;
+import com.niuke.forum.util.ForumUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -12,5 +19,33 @@ public class UserService {
 
     public User getUser(int id) {
         return userDAO.selectById(id);
+    }
+
+    public Map<String, String> register(String userName, String password) {
+        Map<String, String> map = new HashMap<>();
+        if (StringUtils.isEmpty(userName)) {
+            map.put("msg", "用户名不能为空");
+            return map;
+        }
+        if (StringUtils.isEmpty(password)) {
+            map.put("msg", "密码不能为空");
+            return map;
+        }
+
+        User user = userDAO.selectByName(userName);
+        if (user != null) {
+            map.put("msg", "用户名重复");
+            return map;
+        }
+
+        user = new User();
+        user.setName(userName);
+        user.setSalt(UUID.randomUUID().toString().substring(0, 5));
+        user.setHead_url(String.format("http://images.nowcoder.com/head/%dt.png",
+                new Random().nextInt(1000)));
+        user.setPassword(ForumUtil.MD5(password + user.getSalt()));
+        userDAO.addUser(user);
+
+        return map;
     }
 }
